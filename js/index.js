0bbae2ff02d4be090debf54accc1b5b01a14b6d6
shubhubelge <shubhambelge@gmail.com>
2022-05-20 16:22:04 +0530

    inital commit

--- js/index.js
+++ js/index.js
@@ -2,5 +2,12 @@
 // observer.observe();
 // console.log("fdsfds")
 var swiper = new Swiper("#featuredSwiper", {
-
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
 });
+
+$('.vidPayFun').click(function(){
+  console.log("video")
+})
